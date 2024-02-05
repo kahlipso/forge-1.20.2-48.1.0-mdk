b5b99a78cb7e3d274d181aacce0ecd54d76024e2
kahlipso <ggl20172017@outlook.com>
2024-02-05 10:37:36 -0600

    init

--- src/main/java/net/kahlipso/ntmod/NewTrierMod.java
+++ src/main/java/net/kahlipso/ntmod/NewTrierMod.java
@@ -22,7 +22,7 @@ public class NewTrierMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "newtrier_mod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public NewTrierMod()
     {
