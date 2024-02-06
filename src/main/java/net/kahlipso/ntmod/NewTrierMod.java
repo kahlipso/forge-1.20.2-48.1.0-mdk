635f2374af3c663fd39334fd786673d8f36e6036
kahlipso <ggl20172017@outlook.com>
2024-02-06 01:17:16 -0600

    init

--- src/main/java/net/kahlipso/ntmod/NewTrierMod.java
+++ src/main/java/net/kahlipso/ntmod/NewTrierMod.java
@@ -1,6 +1,11 @@
 package net.kahlipso.ntmod;
 
 import com.mojang.logging.LogUtils;
+import net.kahlipso.ntmod.block.ModBlocks;
+import net.kahlipso.ntmod.item.ModCreativeModeTabs;
+import net.kahlipso.ntmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -20,13 +25,18 @@ import org.slf4j.Logger;
 public class NewTrierMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "newtrier_mod";
+    public static final String MOD_ID = "ntmod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public NewTrierMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -45,6 +55,11 @@ public class NewTrierMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/kahlipso/ntmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.kahlipso.ntmod.item;
+
+import net.kahlipso.ntmod.NewTrierMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, NewTrierMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
