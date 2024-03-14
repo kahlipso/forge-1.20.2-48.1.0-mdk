eb5bcaa040ff0f9255705e5b20fa625941836247
kahlipso <ggl20172017@outlook.com>
2024-03-13 21:47:22 -0500

    data gen finish before deleting json files

---
+++ src/main/java/net/kahlipso/ntmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,33 @@
+package net.kahlipso.ntmod.datagen;
+
+import net.kahlipso.ntmod.NewTrierMod;
+import net.kahlipso.ntmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, NewTrierMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.STRAWBERRY);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item)
+    {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(NewTrierMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+}
